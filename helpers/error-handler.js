0817a58349875abe497b0f1eab1e273529049556
Italo Stuardo Cortez Silva <ingeniero.stuardo@gmail.com>
2021-06-18 06:04:51 -0600

    working on backend

---
+++ helpers/error-handler.js
@@ -0,0 +1,12 @@
+function errorHandler(error, req, res, next) {
+  if (error.name === 'UnauthorizedError') {
+    return res.status(401).json({ message: 'User is not authorized' });
+  }
+  if (error.name === 'ValidationError') {
+    return res.status(401).json({ message: error });
+  }
+
+  return res.status(500).json(error);
+}
+
+module.exports = errorHandler;
